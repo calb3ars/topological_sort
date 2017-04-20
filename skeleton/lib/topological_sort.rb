fa0513d7a3f521a4027f754377f1ea70c758adc0
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-04-19 22:33:39 -0700

    Completed specs

---
+++ skeleton/lib/topological_sort.rb
@@ -0,0 +1,26 @@
+require_relative 'graph'
+require 'byebug'
+# Implementing topological sort using both Khan's and Tarian's algorithms
+
+def topological_sort(vertices)
+  sorted = []
+  queue = []
+  vertices.each do |vertex|
+    if vertex.in_edges.empty?
+      queue.push(vertex)
+    end
+  end
+
+  until queue.empty?
+    current = queue.shift
+    sorted << current
+    until current.out_edges.length == 0
+      vertex = current.out_edges[0].to_vertex
+      current.out_edges[0].destroy!
+      if vertex.in_edges.empty?
+        queue.push(vertex)
+      end
+    end
+  end
+  sorted
+end
